2cb28a06c4ec74888412be114dcdbc918f50d901
nallacheruvus <41948894+nallacheruvus@users.noreply.github.com>
2024-08-29 18:16:55 +0530

    Sec Commit

--- src/main/java/com/satish/app/App.java
+++ src/main/java/com/satish/app/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Books b=new Books("Ramayan","Valmiki");
+        System.out.println(b.toString());
     }
 }
